ecd29e7dbb2b2d4c33a4f7d07388ae0203b2f899
Taiyi <qwer23579@163.com>
2016-03-24 11:14:28 +0800

    将扫描类型单独定义为一个cpp

---
+++ AStarSimple.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "AStarSimple.h"
+
+
+AStarSimple::AStarSimple(void)
+{
+}
+
+
+AStarSimple::~AStarSimple(void)
+{
+}
---
+++ AStarSimple.h
@@ -0,0 +1,8 @@
+#pragma once
+class AStarSimple
+{
+public:
+	AStarSimple(void);
+	~AStarSimple(void);
+};
+
